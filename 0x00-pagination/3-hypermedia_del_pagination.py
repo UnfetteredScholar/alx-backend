581e04ade13a1bfd61e04d97b8e63828bc67f385
Ato Toffah <atotoffah@gmail.com>
2024-03-31 17:25:51 +0000

    Task 3:fix 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,39 +40,61 @@ class Server:
         self, index: Union[int, None] = None, page_size: int = 10
     ) -> dict:
         """Gets indexed data from dataset regardless of deleted items"""
-        dataset = self.indexed_dataset()
-        assert (
-            index is not None and index >= 0 and index <= max(dataset.keys())
-        )
+        # dataset = self.indexed_dataset()
+        # assert (
+        #     index is not None and index >= 0 and index <= max(dataset.keys())
+        # )
 
-        data = []
-        count = 0
-        next_index = None
+        # data = []
+        # count = 0
+        # next_index = None
 
-        for i in range(index, len(dataset)):
-            if i in dataset and count < page_size:
-                data.append(dataset[i])
-                count += 1
-                continue
-
-            if count == page_size:
-                next_index = i
-                break
-
-        # for k, v in dataset.items():
-        #     if k >= index and count < page_size:
-        #         data.append(v)
+        # for i in range(index, len(dataset)):
+        #     if i in dataset and count < page_size:
+        #         data.append(dataset[i])
         #         count += 1
         #         continue
 
         #     if count == page_size:
-        #         next_index = k
+        #         next_index = i
         #         break
 
-        hyper = {}
-        hyper["index"] = index
-        hyper["next_index"] = next_index
-        hyper["page_size"] = len(data)
-        hyper["data"] = data
+        # # for k, v in dataset.items():
+        # #     if k >= index and count < page_size:
+        # #         data.append(v)
+        # #         count += 1
+        # #         continue
 
-        return hyper
+        # #     if count == page_size:
+        # #         next_index = k
+        # #         break
+
+        # hyper = {}
+        # hyper["index"] = index
+        # hyper["next_index"] = next_index
+        # hyper["page_size"] = len(data)
+        # hyper["data"] = data
+
+        # return hyper
+        assert (
+            index is not None and index >= 0 and index <= len(self.dataset())
+        )
+
+        indexed_dataset = self.indexed_dataset()
+        indexed_page = {}
+
+        i = index
+        while (len(indexed_page) < page_size and i < len(self.dataset())):
+            if i in indexed_dataset:
+                indexed_page[i] = indexed_dataset[i]
+            i += 1
+
+        page = list(indexed_page.values())
+        page_indices = indexed_page.keys()
+
+        return {
+            'index': index,
+            'next_index': max(page_indices) + 1,
+            'page_size': len(page),
+            'data': page
+        }
