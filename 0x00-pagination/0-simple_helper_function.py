50b65d38ce7ece460ee8bcf47dc7cc721b91b145
Ato Toffah <atotoffah@gmail.com>
2024-03-31 14:16:49 +0000

    Task 0:FIX 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,9 +4,10 @@ Defines the function index_range
 """
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int,int]:
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Generates a tuple containing the start and end indexes 
+    Generates a tuple containing the start and end indexes
     of a range based on the page number and page size
     """
     return ((page_size * page) - page_size, page_size * page)
