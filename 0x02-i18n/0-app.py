ed70e6ee14480252b2f14a91c66b6ce19867e4f1
Ato Toffah <atotoffah@gmail.com>
2024-04-09 16:52:37 +0000

    Task 0:fix 3

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""Task 0: A basic flask application"""
+"""A Basic Flask app.
+"""
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.get("/")
-def index() -> str:
-    """Gets the index page"""
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('0-index.html')
 
-    return render_template("0-index.html")
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
