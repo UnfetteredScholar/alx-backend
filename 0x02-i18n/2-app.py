e1cd88f18d44510fc4fa66b143712853f99f361c
Ato Toffah <atotoffah@gmail.com>
2024-04-09 17:14:21 +0000

    Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Task 1: A Basic Flask app.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -19,10 +19,16 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Gets the best matching locale for a web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route("/")
 def get_index() -> str:
     """Returns the index (home) page"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
