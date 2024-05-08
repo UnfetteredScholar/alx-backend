cff1e6c30e510f34b8d51f92732d80e91fdfe0ec
Ato Toffah <atotoffah@gmail.com>
2024-05-08 16:23:42 +0000

    Task 7

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue({ name: 'push_notification_code' });
 
 const jobData = {
-  phoneNumber: '1113334567',
-  message: 'Account balance updated.',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData);
